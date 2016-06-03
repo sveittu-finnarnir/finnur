25ba8c2916fe05f14e20fc4949e29f7224bdcb40
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-06-03 17:56:08 +0000

    Added more attrs to the users table

--- etc/schema.sql
+++ etc/schema.sql
@@ -2,5 +2,10 @@ CREATE TABLE IF NOT EXISTS "Users" (
     "id" character varying(255),
     "authy_id" character varying(255),
     "arion_token" character varying(255),
-    "wit_session_id" character varying(255)
+    "wit_session_id" character varying(255),
+    "first_name" character varying(255),
+    "last_name" character varying(255),
+    "gender" character varying(10)
 );
+
+INSERT INTO "Users" (id, authy_id) VALUES ('10154234453187128', '23738888');
